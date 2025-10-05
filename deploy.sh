247ce840994687ea9aabf130adbee9e432d569aa
Andre Pereira <andre-luis.pereira@serpro.gov.br>
2025-10-05 11:07:18 -0300

    Corrige a formatação da variável de cor NC no script de deploy do n8n

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ NAMESPACE="n8n"
 
 # Cores para a saída do terminal
 GREEN='\033[0;32m\'
-NC=\'\033[0m\' # No Color
+NC='\033[0m\' # No Color
 
 # Função para verificar se um recurso foi criado
 wait_for_resource() {
